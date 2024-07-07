67d21bfb9338628b896b49fa3c4daafd05d2522e
N1hron <n1hron8008@gmail.com>
2024-07-08 01:25:24 +0300

    Add base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
+import './scss/main.scss';
+
 const root = createRoot(document.getElementById('root')!);
 
 root.render(<StrictMode></StrictMode>);
