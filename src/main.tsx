0ec39a8508462e22450fc5455ce49368ab7dbd44
N1hron <n1hron8008@gmail.com>
2024-07-09 10:55:55 +0300

    Add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+import router from './router';
 
 import './scss/main.scss';
 
 const root = createRoot(document.getElementById('root')!);
 
-root.render(<StrictMode></StrictMode>);
+root.render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
