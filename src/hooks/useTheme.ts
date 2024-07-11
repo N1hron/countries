ccd5f1e8276aa3e060b8f625edc1d07c8f41808a
N1hron <denik8008@gmail.com>
2024-07-11 21:04:37 +0300

    Add useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { setTheme } from '../store/slices/themeSlice';
+import { selectTheme } from '../store/slices/themeSlice/selectors';
+
+function useTheme() {
+    const dispatch = useAppDispatch();
+    const theme = useAppSelector(selectTheme);
+
+    useEffect(() => {
+        document.body.dataset.theme = theme;
+        window.localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    function toggleTheme() {
+        const nextTheme = theme === 'light' ? 'dark' : 'light';
+
+        dispatch(setTheme(nextTheme));
+    }
+
+    return [theme, toggleTheme] as const;
+}
+
+export default useTheme;
