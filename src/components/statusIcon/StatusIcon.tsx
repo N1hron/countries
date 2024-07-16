46a287a0519a3d76b81ac1b71392f3162ed5a8ff
N1hron <n1hron8008@gmail.com>
2024-07-16 20:09:28 +0300

    Add status icon component

---
+++ src/components/statusIcon/StatusIcon.tsx
@@ -0,0 +1,42 @@
+import { SVGProps } from 'react';
+
+import LoadingIcon from '../../assets/icons/reload.svg?react';
+import IdleIcon from '../../assets/icons/info.svg?react';
+import SuccessIcon from '../../assets/icons/checkmark.svg?react';
+import ErrorIcon from '../../assets/icons/cross.svg?react';
+import { Status } from '../../types/status';
+
+import './statusIcon.scss';
+
+interface Props extends SVGProps<SVGSVGElement> {
+    status: Status;
+}
+
+const titles: Record<Status, string> = {
+    error: 'An error occured',
+    idle: 'Useful information',
+    success: 'Success!',
+    loading: 'Loading, please wait',
+};
+
+function StatusIcon({ status, className = '', ...rest }: Props) {
+    const InitialIcon = getInitialIcon();
+    const cn = (`status-icon status-icon_${status} ` + className).trim();
+
+    function getInitialIcon() {
+        switch (status) {
+            case 'idle':
+                return IdleIcon;
+            case 'loading':
+                return LoadingIcon;
+            case 'error':
+                return ErrorIcon;
+            case 'success':
+                return SuccessIcon;
+        }
+    }
+
+    return <InitialIcon {...rest} className={cn} title={titles[status]} />;
+}
+
+export default StatusIcon;
