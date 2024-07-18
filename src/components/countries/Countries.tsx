ef65b059087a92493767fe9b5d4dfbed44471b60
N1hron <n1hron8008@gmail.com>
2024-07-18 15:14:35 +0300

    Add status message to countries component

--- src/components/countries/Countries.tsx
+++ src/components/countries/Countries.tsx
@@ -1,5 +1,6 @@
 import useCountries from '../../hooks/useCountries';
 import Container from '../container/Container';
+import StatusMessage from '../statusMessage/StatusMessage';
 import CountryCard from './CountryCard';
 
 import './countries.scss';
@@ -8,33 +9,31 @@ function Countries() {
     const [status, countries, limit] = useCountries();
 
     function renderCountries() {
-        switch (status) {
-            case 'loading':
-                return 'Loading...';
-            case 'error':
-                return 'Error';
-            case 'idle':
-                return null;
-            case 'success':
-                return countries
-                    .slice(0, limit)
-                    .map((country) => (
-                        <CountryCard
-                            key={country.nativeName}
-                            country={country}
-                        />
-                    ));
-        }
+        return countries
+            .slice(0, limit)
+            .map((country) => (
+                <CountryCard key={country.nativeName} country={country} />
+            ));
     }
 
+    const statusText = status === 'error' ? 'Could not load countries' : '';
+
     return (
-        <Container>
-            <div className='countries'>
-                <div className='countries__country-cards'>
-                    {renderCountries()}
-                </div>
-            </div>
-        </Container>
+        <div className='countries'>
+            <Container className='countries__container'>
+                {status === 'success' ? (
+                    <div className='countries__country-cards'>
+                        {renderCountries()}
+                    </div>
+                ) : (
+                    <StatusMessage
+                        status={status}
+                        statusText={statusText}
+                        className='countries__status-message'
+                    />
+                )}
+            </Container>
+        </div>
     );
 }
 
