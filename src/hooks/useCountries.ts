d809d8c8e47012505860890a9e9aea9c5fbcac52
N1hron <denik8008@gmail.com>
2024-07-25 16:19:31 +0300

    Change useCountries hook

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
+
 import { useAppDispatch, useAppSelector } from '../store';
 import { setLimit } from '../store/slices/countriesSlice';
+import { fetchAllCountries } from '../store/slices/countriesSlice';
 import {
     selectAllCountries,
     selectCountriesStatus,
     selectCountriesLimit,
+    selectCountriesFilter,
 } from '../store/slices/countriesSlice/selectors';
 
 function useCountries() {
@@ -12,15 +16,40 @@ function useCountries() {
     const countries = useAppSelector(selectAllCountries);
     const status = useAppSelector(selectCountriesStatus);
     const limit = useAppSelector(selectCountriesLimit);
+    const filter = useAppSelector(selectCountriesFilter);
 
-    function increaseLimit(n: number = 8) {
-        dispatch(setLimit(limit + n));
+    useEffect(() => {
+        dispatch(fetchAllCountries());
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        dispatch(setLimit(Math.min(filteredCountries.length, 8)));
+    }, [filter]);
+
+    const filteredCountries = countries.filter(
+        ({ region, name }) =>
+            (filter.region === 'All' || region === filter.region) &&
+            name.match(new RegExp(filter.search, 'i'))
+    );
+    const maxLimit = filteredCountries.length;
+
+    function increaseLimit() {
+        let newLimit = limit + 8;
+
+        if (newLimit > maxLimit) {
+            newLimit = maxLimit;
+        } else if (newLimit < 1) {
+            newLimit = Math.min(1, maxLimit);
+        }
+
+        dispatch(setLimit(newLimit));
     }
 
     return {
-        countries: countries.slice(0, limit),
+        countries: filteredCountries.slice(0, limit),
         status,
-        limit: { current: limit, max: countries.length },
+        limit: { current: limit, max: maxLimit },
         increaseLimit,
     };
 }
