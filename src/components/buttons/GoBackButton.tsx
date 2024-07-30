99b4de249de7fcc425f9b879619ee9fa78d02b6c
N1hron <denik8008@gmail.com>
2024-07-30 13:22:00 +0300

    Use useNavigate inside GoBackButton

--- src/components/buttons/GoBackButton.tsx
+++ src/components/buttons/GoBackButton.tsx
@@ -1,15 +1,21 @@
-import { LinkProps } from 'react-router-dom';
+import { ButtonHTMLAttributes } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Button from './button/Button';
 import ArrowBackIcon from '../../assets/icons/arrow-back.svg?react';
 
-function GoBackButton(props: Omit<LinkProps, 'to'>) {
+function GoBackButton({
+    to = -1,
+    ...rest
+}: ButtonHTMLAttributes<HTMLButtonElement> & { to?: number }) {
+    const navigate = useNavigate();
+
     return (
         <Button
-            {...props}
-            style={{ ...props.style, paddingLeft: '1.84rem' }}
-            isLink
-            to='../'
+            {...rest}
+            style={{ ...rest.style, paddingLeft: '1.84rem' }}
+            isLink={false}
+            onClick={() => navigate(to)}
         >
             <ArrowBackIcon />
             Back
