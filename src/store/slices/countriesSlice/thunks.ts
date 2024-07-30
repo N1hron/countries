59f08c34196e7394aac9bad0d96a3289f024d2bf
N1hron <denik8008@gmail.com>
2024-07-30 13:24:36 +0300

    Add condition for fetchAllCountries action

--- src/store/slices/countriesSlice/thunks.ts
+++ src/store/slices/countriesSlice/thunks.ts
@@ -2,9 +2,19 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { CountryInfo } from '../../../types/countries';
 import { Extra } from '../../../types/extra';
+import { RootState } from '../..';
 
 export const fetchAllCountries = createAsyncThunk<
     CountryInfo[],
     undefined,
     { extra: Extra }
->('countries/fetchAll', async (_, { extra: { api } }) => api.getAllCountries());
+>(
+    'countries/fetchAll',
+    async (_, { extra: { api } }) => api.getAllCountries(),
+    {
+        condition: (_, { getState }) => {
+            const { countries } = getState() as RootState;
+            return !countries.entities.all.length;
+        },
+    }
+);
