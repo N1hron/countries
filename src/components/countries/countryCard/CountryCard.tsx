5232a4266a314338ce6f7f11b8635918d0c6ddca
N1hron <denik8008@gmail.com>
2024-08-05 21:45:57 +0300

    Add some animations

--- src/components/countries/countryCard/CountryCard.tsx
+++ src/components/countries/countryCard/CountryCard.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from 'react';
 import { CountryInfo } from '../../../types/countries';
 
 import './countryCard.scss';
@@ -35,12 +36,53 @@ function CountryCard({ country }: Props) {
         );
     };
 
+    const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+    const handleMouseMove: MouseEventHandler<HTMLElement> = (event) => {
+        if (prefersReducedMotion) return;
+
+        const card = event.currentTarget;
+        const cardContent = card.querySelector('.country-card__content') as HTMLDivElement;
+
+        const rect = card.getBoundingClientRect();
+
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const width = rect.width;
+        const height = rect.height;
+
+        const multiplier = 0.075;
+
+        const rotateXDeg = -((y - height / 2) / (height / 2)) * 100 * multiplier;
+        const rotateYDeg = ((x - width / 2) / (width / 2)) * 100 * multiplier;
+
+        const perspective = 45;
+
+        console.log(width / (200 - Math.sin(rotateXDeg) * height));
+        cardContent.style.transform = `perspective(${perspective}rem) rotateY(${rotateYDeg}deg) rotateX(${rotateXDeg}deg)`;
+    };
+
+    const handleMouseLeave: MouseEventHandler<HTMLElement> = (event) => {
+        if (prefersReducedMotion) return;
+
+        const card = event.currentTarget;
+        const cardContent = card.querySelector('.country-card__content') as HTMLDivElement;
+
+        cardContent.style.transform = '';
+    };
+
     return (
-        <article className='country-card'>
-            <img className='country-card__flag' src={country.flag} alt={country.name} />
-            <div className='country-card__info'>
-                <h3 className='country-card__info-title'>{country.name}</h3>
-                <ul className='country-card__info-items'>{renderInfoItems()}</ul>
+        <article
+            className='country-card'
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div className='country-card__content'>
+                <img className='country-card__flag' src={country.flag} alt={country.name} />
+                <div className='country-card__info'>
+                    <h3 className='country-card__info-title'>{country.name}</h3>
+                    <ul className='country-card__info-items'>{renderInfoItems()}</ul>
+                </div>
             </div>
         </article>
     );
